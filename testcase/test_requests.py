ee6d29b0c8fbbbafad650bbde94584bf169cf694
jj1165922611 <1165922611@qq.com>
2021-01-09 21:35:59 +0800

    add cookie

--- testcase/test_requests.py
+++ testcase/test_requests.py
@@ -18,3 +18,17 @@ class TestRequests(object):
         logging.info(r)
         logging.info(r.text)
         logging.info(json.dumps(r.json(), indent=2))
+
+    def test_xueqiu_quote(self):
+        cookie={
+            "xq_a_token":"ad26f3f7a7733dcd164fe15801383e62b6033003"
+        }
+        headers={
+            "host":"xueqiu.com",
+            "User-Agent":"Jo"
+        }
+        r=requests.get("https://xueqiu.com/service/v5/stock/batch/quote?symbol=SH000001%2CSZ399001%2CSZ399006%2CSH000688&_=1610198308617",headers=headers,cookies=cookie)
+        # logging.info(r)
+        # logging.info(r.text)
+        logging.info(json.dumps(r.json(),indent=2))
+        assert r.json()["data"]["items"][0]["quote"]["symbol"] =="SH000001"
